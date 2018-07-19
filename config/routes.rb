d5ea3f01bbe07d33e46cebf8d6d68f8928aef3b0
Wisara8 <srandall845@gmail.com>
2018-07-19 01:40:30 +0000

    user authentication feature added

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
@@ -19,6 +28,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
